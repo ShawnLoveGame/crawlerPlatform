6a831e38158c589c2005f6bf38f2a795b03970a7
alingse <alingse@foxmail.com>
2015-06-06 10:42:47 +0800

    cookie

--- yzm/simple/getimg.py
+++ yzm/simple/getimg.py
@@ -17,7 +17,7 @@ img_header={
     "Accept-Encoding":"gzip, deflate, sdch",
     "Accept-Language":'en-US,en;q=0.8',
     "Connection":"keep-alive",
-    "Cookie":"cck_lasttime=1430182306281; cck_count=2; JSESSIONID=VQqQfPZtdhJyNvpX5TTl46pg8h5YdtgVY9NQnGz4LhflphdrLSzn!-836272584",
+    "Cookie":"JSESSIONID=VycKqrG5TfhHWVywsNyvpl8v2yL9ytGlywmWz8CFQLLLtX5ZpC7n!-836272584",
     "Host":"www.sccredit.gov.cn",
     "Referer":"http://www.sccredit.gov.cn/queryInfo.do?behavior=enterSearch&panel=corp",
     "User-Agent":"MoHozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/41.0.2272.76 Chrome/41.0.2272.76 Safari/537.36" 
@@ -26,8 +26,8 @@ img_header={
 IMGPATH='./test_data/'
 
 def get_imgdata():
-    try:
-        resp=requests.get(Imgurl,headers=img_header)
+    try:        
+	resp=requests.get(Imgurl,headers=img_header)
         return resp.content
     except:
         return None
