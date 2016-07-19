c15c2d842170c5808f7d88f29332c9bf3385c0ee
shibin <shibin@mac>
2016-07-19 13:45:14 +0800

    update:重构 AxB_sorted.py 文件 增加了一个sortedFile 类，优化了while 内逻辑

--- dodata/AxB_sorted.py
+++ dodata/AxB_sorted.py
@@ -1,207 +1,163 @@
+#!/usr/bin/env python2.7
 #coding=utf-8
 #2015/04/16
 #author@shibin
 
-#
-#
-import sys
-from optparse import OptionParser 
+import argparse
 
+'''
+sorted:file A  just like set A
+sorted:file B  just like set B
+'''
 
-def getoptparse():
-	parser = OptionParser() 
-	parser.add_option("-A","--source-A",action="store",
-		dest="soureApath", 
-		default="", 
-		help="soureApath")
-	parser.add_option("-B","--source-B",action="store",
-		dest="soureBpath", 
-		default="", 
-		help="soureBpath")
-	parser.add_option("-x","--xor",action="store",
-		dest="xorpath", 
-		default="", 
-		help="xorpath")
-	parser.add_option("-a","--and",action="store",
-		dest="andpath", 
-		default="", 
-		help="andpath")
-	parser.add_option("--A-B",action="store",
-		dest="A_Bpath", 
-		default="", 
-		help="A_Bpath")
-	parser.add_option("--B-A",action="store",
-		dest="B_Apath", 
-		default="", 
-		help="B_Apath")
-	return parser
-
+#read,(compare,line) from A
 def read_A(f):
 	for line in f:
-		return False,line.strip()
+		return False,(int(line.strip()),line)
 	return True,None
 
+#read,(compare,line) from B
 def read_B(f):
 	for line in f:
-		return False,line.strip()
+		return False,(int(line.strip()),line)
 	return True,None
 
-def compare(a,b):
-	if a==b:
-		return None
-	return a<b
+#default cmp or other cmpare func
+def compare(cmp_a,cmp_b):
+	return cmp(cmp_a,cmp_b)
 
-def write(result,write_types,output_types,outputs):
-	for i in range(4):
-		if write_types[i]==True:
-			if output_types[i]==True:
-				outputs[i].write(result)
+#AND,OR,XOR,A-B,B-A
+#:write a
+A_writes = [False,True,True,True,False]
+#:write b
+B_writes = [False,True,True,False,True]
+#:write equal
+Equal_writes  = [True,True,False,False,False]
 
-def write_Asmall(a,output_types,outputs):
-	result=a+'\n'
-	write_types=(True,None,True,None)
-	write(result,write_types,output_types,outputs)
-
-def write_Bsmall(b,output_types,outputs):
-	result=b+'\n'
-	write_types=(True,None,None,True)
-	write(result,write_types,output_types,outputs)
-
-def write_equal(a,output_types,outputs):
-	#means equal
-	result=a+'\n'
-	write_types=(None,True,None,None)
-	write(result,write_types,output_types,outputs)
+ASC = 'ASC'
+DESC = 'DESC'
+directs = [ASC,DESC]
 
 
-def main(f_a,f_b,output_types,f_outputs):
-	Af=open(f_a,'r')
-	Bf=open(f_b,'r')
-	outputs=[]
-	for i in range(4):
-		if output_types[i]==True:
-			outputs.append(open(f_outputs[i],'w'))
-		else:
-			outputs.append(None)
-
-	a_close=False
-	b_close=False
-	a=None
-	b=None
-	read_a=True	
-	read_b=True
-
-	ct=0
-	while 1:
-		if a_close!=None and read_a:
-			result=read_A(Af)
-			a_close,a=read_A(Af)
-			if a_close==True:
-				Af.close()
-				a_close=None
-		if b_close!=None and read_b:
-			b_close,b=read_B(Bf)
-			if b_close==True:
-				Bf.close()
-				b_close=None
+def write_out(out,writes,outputs):
+	for i in range(len(writes)):
+		if writes[i] == True and outputs[i] != None:
+			outputs[i].write(out)
 
 
-		if a_close==None and b_close==None:
-			#all--read---a,b-is None
+class  SoredFile(object):
+	"""the sorted:file status"""
+	def __init__(self,f,read_f):
+		self.f = f
+		self.read_f = read_f
+		self.closed = False
+		#data == (compare,line)
+		self.data = None
+		self._need_read = True
+		self.count = 0
+
+	def need_read(self):
+		self._need_read = True
+
+	def get_line(self):
+		if self.data != None:
+			return self.data[1]
+		raise Exception("no line data has been read")
+
+	def get_compare(self):
+		if self.data != None:
+			return self.data[1]
+		raise Exception("no line data has been read")
+
+	def read(self):
+		#只有 未读完关闭、并需要读、才去读
+		if self.closed == False and self._need_read == True:
+			close,data = self.read_f(self.f)
+			self.closed = close
+			self._need_read = False		
+			if close == True:
+				self.f.close()
+			else:
+				_cmp,line = data
+			self.data = data
+		return self.closed
+
+
+def operat(f_A,f_B,outputs,direct = ASC):
+	sf_A = SoredFile(f_A,read_A)
+	sf_B = SoredFile(f_B,read_B)
+
+	while True:
+
+		close_A = sf_A.read()
+		close_B = sf_B.read()
+
+		if not close_A and not close_B:
+			AcmpB = compare(sf_A.get_compare(),sf_B.get_compare())
+
+			if AcmpB == 0:
+				writes = Equal_writes
+			else:
+				if (AcmpB == -1 and direct == ASC) or \
+					(AcmpB == 1 and direct == DESC):
+					writes = A_writes
+				else:
+					writes = B_writes
+		elif not close_A and close_B:
+			writes = A_writes
+		elif close_A and not close_B:
+			writes = B_writes
+		elif close_A and close_B:
 			break
-		if a_close==False and b_close==None:
-			#means--b is close
-			write_Asmall(a,output_types,outputs)
-			read_a=True
-			read_b=False
-			continue
-		if a_close==None and b_close==False:
-			#means--a is close
-			write_Bsmall(b,output_types,outputs)
-			read_a=False
-			read_b=True
-			continue
 
-		if a_close==False and b_close==False:
-			#means a & b not None
-			"""
-			#test
-			if a==1670512627:
-				print 'b',b
-				raw_input()
-			if b==1670512627:
-				print 'a',a
-				raw_input()
-			if a==1215854130:
-				print 'b',b
-				raw_input()
-			if b==1215854130:
-				print 'a',a
-				raw_input()
-			"""
-			a_small_b=compare(a,b)
-			ct+=1
-			if ct%100000==0:
-				print 'try 100k:',ct
-			if a_small_b==None:
-				write_equal(a,output_types,outputs)
-				read_a=True
-				read_b=True
 
-			if a_small_b==True:
-				write_Asmall(a,output_types,outputs)
-				#read next a compare to this b 
-				read_a=True
-				read_b=False
-			if a_small_b==False:
-				write_Bsmall(b,output_types,outputs)
-				#read next b compare to this a
-				read_a=False
-				read_b=True
+		if writes == A_writes:
+			out = sf_A.get_line()
+		else:
+			out = sf_B.get_line()
 
-	for i in range(4):
-		if output_types[i]==True:
-			outputs[i].close()
+		write_out(out,writes,outputs)
+
+		if writes == A_writes:
+			sf_A.need_read()
+		elif writes == B_writes:
+			sf_B.need_read()
+		elif writes == Equal_writes:
+			sf_A.need_read()
+			sf_B.need_read()
+
+
+
 
 
 if __name__=="__main__":
-	parser=getoptparse()
-	(options, args) = parser.parse_args()
-	f_a=options.soureApath
-	f_b=options.soureBpath
+	parser = argparse.ArgumentParser()
+	parser.add_argument('A',help='sorted:file A as set A')
+	parser.add_argument('B',help='sorted:file B as set B')
 
-	output_types=[]
-	f_outputs=[]
-	#xor
-	if options.xorpath!="":
-		output_types.append(True)
-		f_outputs.append(options.xorpath)
-	else:
-		output_types.append(None)
-		f_outputs.append(None)
+	parser.add_argument('-x','--xor',help='sorted:file A ^ sorted:file B',action='store')
+	parser.add_argument('-a','--AND','--and',help='sorted:file A & sorted:file B',action='store')
+	parser.add_argument('--A_B',help='sorted:file A - sorted:file B',action='store')
+	parser.add_argument('--B_A',help='sorted:file B - sorted:file A',action='store')
+	parser.add_argument('-o','--OR','--or',help='set A | set B',action='store')	
+	parser.add_argument('-d','--direct',help='direct must be ASC or DESC',default=ASC,choices=[ASC,DESC],action='store')
+	args = parser.parse_args()
 
-	#and
-	if options.andpath!="":
-		output_types.append(True)
-		f_outputs.append(options.andpath)
-	else:
-		output_types.append(None)
-		f_outputs.append(None)
+	f_a = open(args.A,'r')
+	f_b = open(args.B,'r')
 
-	#A-B
-	if options.A_Bpath!="":
-		output_types.append(True)
-		f_outputs.append(options.A_Bpath)
-	else:
-		output_types.append(None)
-		f_outputs.append(None)
+	direct = args.direct.upper()
+	if direct not in directs:
+		print('direct error')
+		exit()
 
-	#B-A
-	if options.B_Apath!="":
-		output_types.append(True)
-		f_outputs.append(options.B_Apath)
-	else:
-		output_types.append(None)
-		f_outputs.append(None)
+	outputs = [args.AND,args.OR,args.xor,args.A_B,args.B_A]
+	for i in range(len(outputs)):
+		f = outputs[i]
+		if f != None:
+			outputs[i] = open(f,'w')
 
-	main(f_a,f_b,output_types,f_outputs)
+	operat(f_a,f_b,outputs,direct)
+	for output in outputs:
+		if output != None:
+			output.close()
