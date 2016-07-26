39d94683fb38349d137d4539423816cf79aace32
alingse <alingse@foxmail.com>
2016-07-26 23:47:20 +0800

    update 修改jsoncsv 的逻辑实现
    大幅度修改，将主要的部分移除到jsoncsv中，利用jsoncsv.py 的结果可以直接得到 xls 文件
        使用方法
        1. python jsoncsv.py raw.json csv.json && python mkexcel.py csv.json output.xls
        2. cat raw.json|python jsoncsv.py|python mkexcel.py > output.xls
        3. python jsoncsv.py raw.json|python mkexcel.py > output.xls

--- dodata/jsoncsv.py
+++ dodata/jsoncsv.py
@@ -3,34 +3,65 @@
 #author@shibin
 #2016.05.27
 
-import sys
 import json
+import sys
 
-def dec_json(jsondoc, head = None):
-    dec_doc = {}
-    for key in jsondoc:
-        value = jsondoc[key]
+def expand_json(json_obj, head = None):
+    exp_obj = {}
+    if type(json_obj) == dict:
+        for key in json_obj:
+            if head == None:
+                k_head = key
+            else:
+                k_head = '{}.{}'.format(head,key)
 
-        k_head = key
-        if head != None:
-            k_head = '{}.{}'.format(head, key)
+            k_obj = json_obj[key]
+            k_exp_obj = expand_json(k_obj,head = k_head)
+            exp_obj.update(k_exp_obj)
+    elif type(json_obj) == list:
+        for i in range(len(json_obj)):
+            if head == None:
+                i_head = str(i)
+            else:
+                i_head = '{}.{}'.format(head,i)
 
-        if type(value) == dict:
-            dec_v = dec_json(value, head = k_head)
-            dec_doc.update(dec_v)
-        elif type(value) == list:
-            for i in range(len(value)):
-                sub_v = value[i]
-                i_head = '{}.{}'.format(k_head, i)
-                dec_v = dec_json(sub_v, head = i_head)
-                dec_doc.update(dec_v)
-        else:
-            dec_doc[k_head] = value
+            i_obj = json_obj[i]
+            i_exp_obj = expand_json(i_obj,head = i_head)
+            exp_obj.update(i_exp_obj)
+    else:
 
-    return dec_doc
+        if head == None:
+            head = ''
+
+        value = json_obj
+        exp_obj[head] = value
+
+    return exp_obj
+   
+
+def contract_json(exp_obj):
+    '''
+    json_obj = {}
+    for key in exp_obj:
+        value = exp_obj[key]
+        keys = key.split('.')
+        this = json_obj
+        for ikey in keys:
+            if ikey.isdigit():
+                pass
+    '''
+    pass
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and sys.argv[1] == '--test':
+        t1 = {'s':{'w':[1,2,3,{'t':5}]}}
+        print(t1)
+        print(expand_json(t1))
+        t2 = [{'1':{'s':[1,2,{'t':[3,{'w':1}]},{'w':1}]}}]
+        print(t2)
+        print(expand_json(t2))
+        exit()
     fin = sys.stdin
     fout = sys.stdout
     if len(sys.argv) >= 2:
@@ -39,7 +70,10 @@ if __name__ == '__main__':
         fout = open(sys.argv[2],'w')
 
     for line in fin:
-        jsondoc = json.loads(line)
-        dec_doc = dec_json(jsondoc)
-        fout.write(json.dumps(dec_doc,ensure_ascii=False).encode('utf-8'))
+        json_obj = json.loads(line)
+        exp_obj = expand_json(json_obj)
+        fout.write(json.dumps(exp_obj,ensure_ascii=False).encode('utf-8'))
         fout.write('\n')
+
+
+
