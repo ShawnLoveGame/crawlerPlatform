3765758f2b05657c7b2c2c3105913589ea247f73
alingse <alingse@foxmail.com>
2016-08-30 19:59:43 +0800

    add:cookie

--- projects/taobao.com/get_item_start.py
+++ projects/taobao.com/get_item_start.py
@@ -43,12 +43,12 @@ def get_detail(item_id = '525528798012'):
         "accept-language": "zh-CN,zh;q=0.8,en;q=0.6", 
         "accept-encoding": "gzip, deflate, sdch, br", 
         "accept": "*/*", 
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36", 
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36", 
+        "cookie": "v=0; cookie2=1c49d1dc6d737d1a8b6634834559b408; t=ad5d4bfe7e49d8a230e9ce8f4da70b7c; cna=vttLEARrqxsCAbaUQzOSYVab; thw=cn; _tb_token_=e5eb36e615eeb; uc1=cookie14=UoWwJr6kOgb0TQ%3D%3D; isg=AldXelRzzGuB40jmdAvmuP9K5s0WHCv-RpgHPamEcyaN2HcasWy7ThX6TM-8; mt=ci%3D-1_0; l=Al5e5tyWo3ZiYcWFkBgtVLjDLv6gJiOq", 
         "pragma": "no-cache", 
         "cache-control": "no-cache", 
-        "referer": "https://detail.tmall.com/item.htm?id={}".format(item_id)
+        "referer": "https://detail.tmall.com/item.htm?spm=a230r.1.14.89.xohYSn&id={}&ns=1&abbucket=1".format(item_id)
     }
-
     #这里是请求数据的
     try:
         #params query数据，headers，request 头数据
